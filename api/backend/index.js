51fe1309f05882b08c0a51fce5290ea9ab8504bd
JaedonM23 <2544456@students.wits.ac.za>
2024-04-10 19:28:31 +0200

    first commit

---
+++ api/backend/index.js
@@ -0,0 +1,22 @@
+module.exports = async function (context, req) {
+    
+    if(req.method==="GET"){
+        context.res.json(cars);
+        
+    }
+    
+    else if (req.method==="POST"){
+        if (req.headers.function === "add"){
+            console.log(req);
+            const newCar = req.body;
+            console.log(newCar);
+            cars.push(newCar);
+            context.res.json(newCar);
+        }
+        else if (req.headers.function === "delete"){
+            const id = req.body.id;
+            cars.splice(id, 1);
+            context.res.json({ message: `Car with id ${id} deleted` });
+        }
+    }
+}
